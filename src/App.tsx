2bf93cca879a9f59100a4d126f4211498dd9ff33
Igor <(igorrds1998@gmail.com)>
2022-07-19 03:31:00 -0300

    criando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,27 @@
-import { FormAdd } from "./components/FormAdd";
-import { UsePeopleList } from "./reducers/peopleList";
+import {Routes, Route} from 'react-router-dom';
+import { Home } from './pages/home';
+import { About } from './pages/about';
 
 const App = () => {
-  const [list, listDispatch] = UsePeopleList();
-
-  const handleAddForm = (inputName: string) => {
-    listDispatch({
-      type: 'ADD',
-      payload: {
-        name: inputName
-      }
-    })
-  }
-
-  const deletePerson = (id: string) => {
-    listDispatch({
-      type: 'DEL',
-      payload: { id }
-    });
-  }
-
-  const handleOrderButton = () => {
-    listDispatch({
-      type: 'ORDER'
-    });
-  }
-
-  return ( 
-    <div>
+  return (
+    <div className="p-4">
+      <header>
+      <h1>Titulo do site</h1>
+      </header>
       <hr/>
+      <div className="py-4">
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<About />} />
+        </Routes>
 
-      <FormAdd onData={handleAddForm}/>
-      
-      <button className="border-solid border-2 text-[20px] p-1" onClick={handleOrderButton}>Ordenar</button>
-      <br/>
-      Lista de pessoas:
-      <ul>
-        {list.map((item, index) => (
-          <li key={index}>{item.name}
-          <button className="border-1 bg-red-600 p-0.25 ml-3" onClick={() => deletePerson(item.id)}>[ Remove ]</button>
-          
-          </li>     
-        ))}
-      </ul>
+      </div>
+      <hr/>
+      <footer>
+      Todos os direitos reservados.
+      </footer>
     </div>
   );
 }
-
 export default App;
